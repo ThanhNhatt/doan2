0d73d876111972496f9d5ce6ca7728c198578c4d
your name <thanhnhathc2004@gnmail.com>
2023-12-06 14:38:01 +0700

    first commit

---
+++ mvc/Views/User/blocks/Footer.php
@@ -0,0 +1,89 @@
+<footer class="Footer">
+    <a href="./TrangChu" class="image-logo--footer">
+        <img class="Footer__Image" src="Public/image/logooo.png" alt="">
+    </a>
+    <div class="Footer__Contain">
+        <ul>
+            <h4>Thông tin liên hệ</h4>
+            <li>
+                <p class=""><i class="fa-solid fa-location-dot"></i>Trường ĐHCNTT&TT Việt Hàn,Ngũ Hành Sơn, Đà Nẵng,Việt Nam</p>
+            </li>
+        </ul>
+
+        <ul>
+            <h4>Hổ trợ</h4>
+            <li>
+                <i class="fa-solid fa-phone"></i>
+                <span>0971633396</span>
+            </li>
+            <li>
+                <i class="fa-solid fa-phone"></i>
+                <span>0376870004</span>
+            </li>
+            <li>
+                <i class="fa-solid fa-envelope"></i>
+                <span>thanhnhathc2004@gmail.com</span>
+            </li>
+            <li>
+                <i class="fa-solid fa-clock"></i>
+                <span>T2 - T6 ( 9h -> 17h30 )</span>
+            </li>
+        </ul>
+
+        <ul>
+            <h4>Dịch vụ khách hàng</h4>
+            <li>
+                <a href="">Tìm kiếm</a>
+            </li>
+            <li>
+                <a href="">Chính sách đổi trả</a>
+            </li>
+            <li>
+                <a href="">Chính sách bảo mật</a>
+            </li>
+            <li>
+                <a href="">Điều khoản dịch vụ</a>
+            </li>
+        </ul>
+
+        <ul>
+            <h4>Về chúng tôi</h4>
+            <li>
+                <a href="">Tin mới</a>
+            </li>
+            <li>
+                <a href="">Khuyến mại mới nhất tại cửa hàng</a>
+            </li>
+            <li>
+                <a href="">Địa điểm cửa hàng Suplo Beauty</a>
+            </li>
+            <li>
+                <a href="">Câu lạc bộ làm đẹp</a>
+            </li>
+        </ul>
+
+        <ul>
+            <h4>ĐĂNG KÝ BẢN TIN</h4>
+            <li>
+                <p>
+                    Hãy nhập địa chỉ email của bạn vào ô dưới đây để có thể nhận được tất cả các tin tức mới nhất của Suplo về các sản phẩm mới, các chương trình khuyến mãi mới. Suplo xin đảm bảo sẽ không gửi mail rác, mail spam tới bạn.
+                </p>
+            </li>
+            <li>
+                <form method="POST" action="" class="form__Footer">
+                    <input type="text" class="Email__Footer" placeholder="Nhập email của bạn">
+                    <input type="submit" class="submit__Footer" value="Đăng ký">
+                </form>
+
+            </li>
+            <li>
+                <div class="Icon__Footer">
+                    <i class="fa-brands fa-facebook"></i>
+                    <i class="fa-brands fa-twitter"></i>
+                    <i class="fa-brands fa-instagram"></i>
+                    <i class="fa-brands fa-youtube"></i>
+                </div>
+            </li>
+        </ul>
+    </div>
+</footer>
